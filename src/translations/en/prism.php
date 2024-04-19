ae58ba202756efcf5bd9f14fc95b1a463ae42623
Josh Crawford <joshua@sgroup.com.au>
2024-04-19 22:17:34 +1000

    Update English translations.

--- src/translations/en/prism.php
+++ src/translations/en/prism.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+  'Configure the default syntax highlighting options' => 'Configure the default syntax highlighting options',
+  'Custom Settings' => 'Custom Settings',
+  'General Settings' => 'General Settings',
+  'Prism Syntax Highlighting' => 'Prism Syntax Highlighting',
+  'Set a directory path for your custom CSS. You‘ll need to edit the plugin config to set which custom themes are selectable.' => 'Set a directory path for your custom CSS. You‘ll need to edit the plugin config to set which custom themes are selectable.',
 ];
